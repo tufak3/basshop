9ebe88e1a1c4c6742fafa5d1f67a47f04f37fa84
tufak3 <dobugray24@yandex.ru>
2025-02-24 20:35:08 +0300

    created half navbar

---
+++ bassshop/src/navbar.jsx
@@ -0,0 +1,60 @@
+import './navbar.css'
+
+import shopLogo from '/Logo.svg'
+import searchIcon from '/Search.svg'
+import profileIcon from '/profile.svg'
+import favoritesIcon from '/favorite.svg'
+import cartIcon from '/cart.svg'
+
+
+function Navbar(){
+    return(
+        <>
+        <div className='nav-container'>
+            <div className='navbar'>
+                <div className='logo'>
+                    <img src={shopLogo} alt="logo" className='logo-img' />
+                    
+                </div>
+                <div className='name'>
+                <p>BASS <br></br> SHOP</p>
+                </div>
+                <div className='input-cont'>
+                    <div className="search-wrapper">
+                        <img src={searchIcon} alt="search" className="search-icon" />
+                        <input 
+                            type="text" 
+                            placeholder='Просто начните вводить название' 
+                            className='input-text'
+                        />
+                    </div>
+                </div>
+                <div className='contacts'>
+                    <div className='contacts-button'>
+                        <button className='contacts-button-text'>Контакты</button>
+                    </div>
+                    <div className='favorites-button'>
+                        <a href="/favorites">
+                            <img src={favoritesIcon} alt="Избранное" />
+                        </a>
+                    </div>
+                    <div className='cart-button'>
+                        <a href="/cart">
+                            <img src={cartIcon} alt="Корзина" />
+                        </a>
+                    </div>
+                    <div className='profile-button'>
+                        <a href="/profile">
+                            <img src={profileIcon} alt="Профиль" />
+                        </a>
+                    </div>
+                </div>
+                <div className='socials'>
+
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Navbar
